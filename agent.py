c6dabab1b06342691f66dd871774604b189a5d17
longnn26 <109263957+lieudvq-hisoft@users.noreply.github.com>
2024-12-11 08:29:37 +0700

    Feat(@agent): Defining the Chatbot scope

--- agent.py
+++ agent.py
@@ -11,6 +11,7 @@ from langchain_community.chat_message_histories import Neo4jChatMessageHistory
 from langchain.agents import AgentExecutor, create_react_agent
 from langchain_core.runnables.history import RunnableWithMessageHistory
 from langchain import hub
+from langchain_core.prompts import PromptTemplate
 
 from utils import get_session_id
 
@@ -34,7 +35,44 @@ tools = [
 def get_memory(session_id):
     return Neo4jChatMessageHistory(session_id=session_id, graph=graph)
 
-agent_prompt = hub.pull("hwchase17/react-chat")
+agent_prompt = PromptTemplate.from_template("""
+You are a software engineer expert providing information about Flutter.
+Be as helpful as possible and return as much information as possible.
+Do not answer any questions that do not relate to Flutter.
+
+Do not answer any questions using your pre-trained knowledge, only use the information provided in the context.
+
+TOOLS:
+------
+
+You have access to the following tools:
+
+{tools}
+
+To use a tool, please use the following format:
+
+```
+Thought: Do I need to use a tool? Yes
+Action: the action to take, should be one of [{tool_names}]
+Action Input: the input to the action
+Observation: the result of the action
+```
+
+When you have a response to say to the Human, or if you do not need to use a tool, you MUST use the format:
+
+```
+Thought: Do I need to use a tool? No
+Final Answer: [your response here]
+```
+
+Begin!
+
+Previous conversation history:
+{chat_history}
+
+New input: {input}
+{agent_scratchpad}
+""")
 agent = create_react_agent(llm, tools, agent_prompt)
 agent_executor = AgentExecutor(
     agent=agent,
