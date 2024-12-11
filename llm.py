0a33d2b649c32bbf6b031448fe579c4a3668607c
longnn26 <109263957+lieudvq-hisoft@users.noreply.github.com>
2024-12-11 08:20:21 +0700

    Config(@app): Project Configuration

--- llm.py
+++ llm.py
@@ -1,6 +1,9 @@
 import streamlit as st
+from langchain_ollama import ChatOllama
+from langchain_community.embeddings import OllamaEmbeddings
 
 # Create the LLM
-
+llm = ChatOllama(model="llama3.1:8b")
 
 # Create the Embedding model
+embeddings = OllamaEmbeddings(model="nomic-embed-text:latest")
