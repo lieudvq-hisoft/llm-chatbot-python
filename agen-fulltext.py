ec505c1b89ff7d29cae75422020ceebb49e787cd
longnn26 <109263957+lieudvq-hisoft@users.noreply.github.com>
2024-12-12 18:24:09 +0700

    Update agen-fulltext.py

--- agen-fulltext.py
+++ agen-fulltext.py
@@ -110,6 +110,7 @@ template = """Answer the question based only on the following context:
 Question: {question}
 Use natural language and be concise.
 Answer:"""
+
 prompt = ChatPromptTemplate.from_template(template)
 
 chain = (
